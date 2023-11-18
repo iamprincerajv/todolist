7300c3b661f44ae029f06aa25f7504943939c0f9
Prince <workwithprince2022@gmail.com>
2023-11-18 22:27:16 +0530

    UI changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className='flex sticky bg-orange-700 text-white justify-start items-center h-12'>
       <p className='text-2xl ms-4 sm:ms-8 font-bold'>AcTo</p>
-      <ul className='flex justify-evenly w-96'>
+      <ul className='flex justify-evenly w-full sm:w-96 text-sm'>
         <li className={`py-1 px-3 rounded ${pathname === "/" ? "bg-orange-900" : ""}`} > <Link href="/">Home</Link> </li>
         <li className={`py-1 px-3 rounded ${pathname === "/addtodo" ? "bg-orange-900" : ""}`} > <Link href="/addtodo">Add</Link> </li>
         <li className={`py-1 px-3 rounded ${pathname === "/history" ? "bg-orange-900" : ""}`} > <Link href="/history">History</Link> </li>
